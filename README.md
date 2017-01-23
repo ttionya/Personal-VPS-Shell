11a9a40215041fbd7886c1d8eab07a6f2bd9e7bf
ttionya <git@ttionya.com>
2017-01-23 15:58:51 +0800

    install_zsh Ver1.0.0

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ### **自用服务器脚本**
 
-##### [upgrade_apache.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/upgrade_apache.sh)
+#### [upgrade_apache.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/upgrade_apache.sh)
 - 仅支持 CentOS 6.X、Apache 2.4、PCRE 1
 
-##### [upgrade_php.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/upgrade_php.sh)
+#### [upgrade_php.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/upgrade_php.sh)
 - 仅支持 CentOS 6.X、PHP 7.0.X / PHP 7.1.X、Apache 2.4、PCRE 1
 
-##### [install_nvm.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/install_nvm.sh)
+#### [install_nvm.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/install_nvm.sh)
 - 理论支持全部 CentOS 版本，但仅在 CentOS 6.8 下进行过测试
 - 涉及 `rm -rf` 命令，操作务必小心
 - 已将软件源设为淘宝，歪果仁慎用
+
+#### [install_zsh.sh](https://raw.githubusercontent.com/ttionya/Personal-VPS-Shell/master/install_zsh.sh)
+- 暂时只支持 yum, apt, zypper, pacman 包管理器
---
+++ install_zsh.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# Version: 1.0.0
+# Author: ttionya
+
+
+################### Custom Setting ####################
+# 执行本脚本的用户，用 `whoami` 查看
+Current_User="root"
+# Zsh Theme (https://wiki.github.com/robbyrussell/oh-my-zsh/themes)
+Zsh_Theme="ys"
+# Zsh Plugins 用空格隔开 (https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins)
+Zsh_Plugins="git composer docker encode64 extract grunt gulp history ng node npm nvm python redis-cli screen sudo yum yarn z"
+
+################### Check Info Start ####################
+# Check root User
+if [ $EUID != 0 ]; then
+   echo "错误：该脚本必须以 root 身份运行"
+   exit 1
+fi
+
+# Check Current User
+Current_User_Info=`cat /etc/passwd | grep -oE "^$Current_User:(.*)"`
+if [ -z $Current_User_Info ]; then
+    echo "执行本脚本的用户不存在"
+    exit 1
+fi
+
+# Check System
+PM=""
+which yum 1>/dev/null 2>/dev/null && PM="yum"
+which apt-get 1>/dev/null 2>/dev/null && PM="apt"
+which zypper 1>/dev/null 2>/dev/null && PM="zypper"
+which pacman 1>/dev/null 2>/dev/null && PM="pacman"
+if [ -z "$PM" ]; then
+    echo "错误：该脚本仅支持 yum, apt, zypper, pacman 包管理器"
+    exit 1
+fi
+################### Check Info End ####################
+
+
+# Main Function
+function main() {
+    echo ""
+    echo "===================== Oh My Zsh 安装程序 启动 ===================="
+
+    # Install zsh
+    case $PM in
+    yum)
+        yum install -y zsh
+        ;;
+    apt)
+        apt-get install -y zsh
+        ;;
+    zypper)
+        zypper install --no-confirm zsh
+        ;;
+    pacman)
+        pacman -S zsh
+        ;;
+    esac
+
+    # Switch User
+    su - $Current_User <<HERE
+
+        # Install Oh My Zsh
+        echo ""
+        echo "开始下载 Oh My Zsh..."
+        echo ""
+
+        wget -c -t3 -T60 -qO- "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | bash
+        if [ $? != 0 ]; then
+            echo "Oh My Zsh 安装失败"
+            exit 1
+        fi
+        echo "安装成功，正在进行配置..."
+
+        # Setting ~/.zshrc
+        if [ -f ~/.zshrc ]; then
+            sed -i 's/^ZSH_THEME=.*/ZSH_THEME="$Zsh_Theme"/' ~/.zshrc
+            sed -i 's/^plugins=.*/plugins=($Zsh_Plugins)/' ~/.zshrc
+            source ~/.zshrc
+        fi
+
+        echo "===================== Oh My Zsh 安装配置完成 ===================="
+    exit
+HERE
+}
+
+# Show Install Information
+clear
+echo "##########################################################"
+echo "# Auto Install Script for Oh My Zsh                      #"
+echo "# Author: ttionya                                        #"
+echo "##########################################################"
+echo ""
+echo "将安装 Oh My Zsh"
+echo ""
+echo "是否安装？ (y/n)"
+read -p "(Default: n):" Check_Install
+if [ -z $Check_Install ]; then
+    Check_Install="n"
+fi
+
+# Check Install
+if [[ $Check_Install == y || $Check_Install == Y ]]; then
+    main
+else
+    echo ""
+    echo "Oh My Zsh 安装被取消，未作任何更改..."
+    echo ""
+fi
+
+
+# 删除 oh-my-zsh
+# uninstall_oh_my_zsh

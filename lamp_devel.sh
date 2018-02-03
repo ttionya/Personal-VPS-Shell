585ae98f22a05625cd0a8ba02daf6ef66923e7b4
ttionya <git@ttionya.com>
2018-02-03 17:53:41 +0800

    lamp_devel Ver1.0.0

---
+++ lamp_devel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Version: 1.0.0
+# Author: ttionya
+
+yum -y install \
+autoconf automake \
+bzip2 bzip2-devel \
+cpp curl curl-devel cmake \
+freetype freetype-devel \
+gcc gcc-c++ gd glibc glibc-devel gettext gettext-devel gmp gmp-devel \
+lynx lua-devel libicu-devel libtool libjpeg-devel libpng-devel libxslt-devel libxml2-devel libwebp-devel \
+make openldap openldap-devel openssl openssl-devel pam-devel \
+vim wget unzip zip zlib zlib-devel

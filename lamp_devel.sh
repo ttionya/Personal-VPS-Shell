42a45dc9444764038d6a855980eb1e0fa7c21fe1
ttionya <git@ttionya.com>
2018-02-17 17:27:44 +0800

    lamp_devel Ver1.0.1

--- lamp_devel.sh
+++ lamp_devel.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Version: 1.0.0
+# Version: 1.0.1
 # Author: ttionya
 
 yum -y install \
 autoconf automake \
-bzip2 bzip2-devel \
+bison bison-devel bzip2 bzip2-devel \
 cpp curl curl-devel cmake \
 freetype freetype-devel \
 gcc gcc-c++ gd glibc glibc-devel gettext gettext-devel gmp gmp-devel \
 lynx lua-devel libicu-devel libtool libjpeg-devel libpng-devel libxslt-devel libxml2-devel libwebp-devel \
-make openldap openldap-devel openssl openssl-devel pam-devel \
+make ncurses-devel \
+openldap openldap-devel openssl openssl-devel pam-devel readline-devel \
 vim wget unzip zip zlib zlib-devel
+
+# Ver1.0.1
+# - 添加一些 PHP 依赖

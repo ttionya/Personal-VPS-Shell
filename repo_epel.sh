e5137f3b6b2c872225f9c42cbdad857d45431d46
ttionya <git@ttionya.com>
2019-02-15 17:24:44 +0800

    repo_epel Ver1.0.2

--- repo_epel.sh
+++ repo_epel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version: 1.0.1
+# Version: 1.0.2
 # Author: ttionya
 
 
@@ -33,20 +33,20 @@ function color() {
 
 ################### Check Info Start ####################
 # Check root User
-if [ $EUID != 0 ]; then
+if [[ ${EUID} != 0 ]]; then
    color red "错误：该脚本必须以 root 身份运行"
    exit 1
 fi
 
 # Check CentOS Version
 # CentOS 7.X Only
-if [ -s /etc/redhat-release ]; then
+if [[ -s /etc/redhat-release ]]; then
     CentOS_Ver=`grep -oE  "[0-9.]+" /etc/redhat-release`
 else
     CentOS_Ver=`grep -oE  "[0-9.]+" /etc/issue`
 fi
 CentOS_Ver=${CentOS_Ver%%.*}
-if [ $CentOS_Ver != 7 ]; then
+if [[ ${CentOS_Ver} != 7 ]]; then
     color red "错误：该脚本仅支持 CentOS 7.X 版本"
     exit 1
 fi
@@ -58,14 +58,14 @@ function install_epel() {
     color yellow "==================== 开始安装 EPEL Repo ===================="
 
     yum -y install epel-release
-    if [ $? != 0 ]; then
+    if [[ $? != 0 ]]; then
         color red "错误：EPEL 安装失败"
         exit 1
     fi
     rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
 
     # China
-    if [[ $In_China == 1 ]]; then
+    if [[ ${In_China} == 1 ]]; then
         sed -i 's@^\(#\)\?baseurl=.*\.org\/pub\/epel\/\(.*\)@baseurl=https://mirrors.tuna.tsinghua.edu.cn/epel/\2@' /etc/yum.repos.d/epel.repo
         color green "已将清华源作为 EPEL 源"
     fi
@@ -77,7 +77,7 @@ function install_epel() {
 # Check EPEL
 Is_Installed_EPEL=`yum list installed | grep epel-release | wc -l`
 
-if [[ $Is_Installed_EPEL == 1 ]]; then
+if [[ ${Is_Installed_EPEL} == 1 ]]; then
     color yellow "检测到已安装 EPEL Repo，跳过..."
 else
     color yellow "检测到未安装 EPEL Repo，即将安装..."
@@ -86,3 +86,6 @@ fi
 
 # Ver1.0.1
 # - 安装完成后加载 GPG Key
+#
+# Ver1.0.2
+# - 优化脚本

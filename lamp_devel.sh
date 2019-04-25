5d5203689d1e3bb302ea8b7e9c86c0e899283f2a
ttionya <git@ttionya.com>
2019-04-25 23:35:19 +0800

    lamp_devel Ver1.0.4

--- lamp_devel.sh
+++ lamp_devel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version: 1.0.3
+# Version: 1.0.4
 # Author: ttionya
 
 yum -y install \
@@ -9,7 +9,7 @@ bison bison-devel bzip2 bzip2-devel \
 cpp curl curl-devel cmake \
 freetype freetype-devel \
 gcc gcc-c++ gd glibc glibc-devel gettext gettext-devel gmp gmp-devel \
-lynx lua-devel libicu-devel libtool libjpeg-devel libpng-devel libxslt-devel libxml2-devel libwebp-devel libzip-devel \
+lynx lua-devel libicu-devel libtool libjpeg-devel libpng-devel libxslt-devel libxml2-devel libwebp-devel \
 make ncurses-devel \
 openldap openldap-devel openssl openssl-devel pam-devel perl-core readline-devel \
 vim wget unzip zip zlib zlib-devel
@@ -22,3 +22,6 @@ vim wget unzip zip zlib zlib-devel
 #
 # Ver1.0.3
 # - 添加 PHP 7.3 依赖
+#
+# Ver1.0.4
+# - PHP 7.3 需要 0.11 以上版本，故移除 libzip (0.10) 依赖
